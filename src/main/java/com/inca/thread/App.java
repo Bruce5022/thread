5e66cc7b6e13fa2ca152e91aef3073e4f22fa580
史战伟 <shizhanwei123@126.com>
2018-09-25 05:45:16 +0800

    创建线程学习仓库
    Signed-off-by: 史战伟 <shizhanwei123@126.com>

---
+++ src/main/java/com/inca/thread/App.java
@@ -0,0 +1,13 @@
+package com.inca.thread;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
